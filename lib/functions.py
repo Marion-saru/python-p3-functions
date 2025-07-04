2d33d00402f98a6c06fa31ae2e3dda8cfe950993
Marion Saru <maghangasaru@gmail.com>
2025-07-04 21:20:15 +0300

    Completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    return f"Hello, {name}!"
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    else:
+        return f"{number / 2:.2f}"
+
+greet_programmer()
+
+func_greet = greet("Marion")
+print(func_greet)
+
+greet_with_default()
+
+sum = add(5,5)
+print(sum)
+
+halve_value = halve(10)
+print(halve_value)
